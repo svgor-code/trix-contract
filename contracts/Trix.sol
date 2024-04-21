7f0a696281ca1e617831450bb43e941bf9c3a71c
Konstantin Troitskiy <atr.konst@gmail.com>
2024-04-21 22:18:01 +0200

    added function erc-20 donation

--- contracts/Trix.sol
+++ contracts/Trix.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
+interface IERC20 {
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+}
+
 contract Trix {
     address owner;
     uint256 internal constant MAX_FEE = 1; // 1%
@@ -11,17 +24,19 @@ contract Trix {
 
     event Donat(
         address from,
-        address to,
+        address indexed to,
         uint256 amount,
-        uint256 timestamp,
+        uint256 indexed timestamp,
         string username,
-        string message
+        string message,
+        address indexed token
     );
 
     function sendDonation(
         address _to,
         string memory _username,
-        string memory _message
+        string memory _message,
+        address _token
     ) external payable {
         require(_to != address(0), "Unexpected zero address");
         require(msg.value > 0, "Donation can't be equal zero");
@@ -29,8 +44,12 @@ contract Trix {
         uint256 fee = (msg.value * MAX_FEE) / 100;
         uint256 amount = msg.value - fee;
 
-        payable(owner).transfer(fee);
-        payable(_to).transfer(amount);
+        if (_token != address(0)) {
+            _sendTokenDonation(_to, _token, fee, amount);
+        } else {
+            payable(owner).transfer(fee);
+            payable(_to).transfer(amount);
+        }
 
         emit Donat(
             msg.sender,
@@ -38,7 +57,19 @@ contract Trix {
             msg.value,
             block.timestamp,
             _username,
-            _message
+            _message,
+            _token
         );
+    }
+
+    function _sendTokenDonation(
+        address _to,
+        address _token,
+        uint256 _fee,
+        uint256 _amount
+    ) internal {
+        IERC20 token = IERC20(_token);
+        token.transferFrom(msg.sender, owner, _fee);
+        token.transferFrom(msg.sender, _to, _amount);
     }
 }
